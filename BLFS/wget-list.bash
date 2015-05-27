b485ca32c192e85bb112ddf5b891fd6dc53efd46
John Freeman <john@brainkernel.com>
2015-05-27 00:00:12 -0500

    Moving the BLFS directory to its own git

--- BLFS/wget-list.bash
+++
@@ -1,6 +0,0 @@
-http://downloads.sourceforge.net/cracklib/cracklib-2.9.2.tar.gz
-http://downloads.sourceforge.net/cracklib/cracklib-words-20080507.gz
-http://downloads.sourceforge.net/libpng/libpng-1.6.16.tar.xz
-http://downloads.sourceforge.net/libpng-apng/libpng-1.6.16-apng.patch.gz
-ftp://space.mit.edu/pub/davis/slang/v2.2/slang-2.2.4.tar.bz2
-http://ftp.gnu.org/gnu/nano/nano-2.3.6.tar.gz
